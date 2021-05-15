65ca487b37acda6f3252f70150c61e89d30eb98b
Solda <solda@khu.ac.kr>
2021-05-15 18:27:03 +0900

    Update the files

---
+++ Challenge/2o1da/everyArray/solve.js
@@ -0,0 +1,8 @@
+const inputArray = [2, 4, 6, 8, 10];
+
+// write your codes
+const answer = inputArrayw.every(function (e) {
+  return e % 2 == 0;
+});
+
+console.log(answer);
---
+++ Challenge/2o1da/expDivOdd/solve.js
@@ -0,0 +1,3 @@
+const inputArray = [1, 7, 3, 4, 6];
+
+// write your codes
